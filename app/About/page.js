15bb83dc2192dc45b250ce34ba2dfe166e033094
Muhammad Amir <umairamir1127@gmail.com>
2024-08-07 18:38:29 -0400

    setup of pages

---
+++ app/About/page.js
@@ -0,0 +1,11 @@
+import Header from './components/header';
+import Footer from './components/footer';
+import Aboutpage from './pages/aboutpage';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  )
+}
--- app/Bot/page.js
+++ app/Bot/page.js
@@ -1,72 +1,11 @@
-'use client';
+import Header from './components/header';
+import Footer from './components/footer';
+import Botpage from './pages/botpage';
 
-import { useState } from 'react';
-
-export default function ChatBot() {
-  const [userInput, setUserInput] = useState('');
-  const [chatHistory, setChatHistory] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleUserInput = async () => {
-    setIsLoading(true);
-
-    setChatHistory((prevChat) => [
-      ...prevChat,
-      { role: 'user', content: userInput },
-    ]);
-
-    const chatCompletion = await openai.chat.completions.create({
-      messages: [...chatHistory, { role: 'assistant', content: userInput }],
-      model: 'gpt-3.5-turbo',
-    });
-
-    setChatHistory((prevChat) => [
-      ...prevChat,
-      { role: 'assistant', content: chatCompletion.choices[0].message.content },
-    ])
-
-    setUserInput('');
-    setIsLoading(false);
-  }
-
+export default function Bot() {
   return (
-    <div className="chat-container">
-      <div className="chat-box">
-        <div className="chat-title">
-          ChatterBot
-        </div>
-        <p className="chat-description">  </p>
-
-        <div className="chat-history">
-          {chatHistory.map((message, index) => (
-            <div
-              key={index}
-              className={`chat-message ${message.role}`}
-            >
-              <div className={`message-sender ${message.role}`}>
-                {message.role === 'user' ? 'User' : 'ChatterBot'}
-              </div>
-              <div className={`message-content ${message.role}`}>
-                {message.content}
-              </div>
-            </div>
-          ))}
-        </div>
-
-        <div className="input-area">
-          <input
-            type="text"
-            placeholder="Ask me anything"
-            value={userInput}
-            onChange={(e) => setUserInput(e.target.value)}
-          />
-          {isLoading ? (
-            <div className="loading">Loading..</div>
-          ) : (
-            <button onClick={handleUserInput}>Ask</button>
-          )}
-        </div>
-      </div>
+    <div>
+      <h1>Bot</h1>
     </div>
   )
 }
