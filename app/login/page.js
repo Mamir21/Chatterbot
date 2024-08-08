aea5ad94836525073b3f1ed068aad1cf83389f77
Mir Shahiduzzaman <mshahiduzzaman@fordham.edu>
2024-08-08 09:21:46 -0400

    Added Gmail Login Button

--- app/login/page.js
+++ app/login/page.js
@@ -1,11 +1,23 @@
+'use client'
 // import Header from '../components/header';
 // import Footer from '../components/footer';
 // import Home from '../components/homepage';
+import { Box, TextField, Button } from "@mui/material";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {auth} from '../../firebase.js'
 
 export default function Login() {
+    const handleGoogle = async (e) => {
+        const provider = new GoogleAuthProvider();
+        return signInWithPopup(auth, provider);
+    }
+
     return (
-      <div>
-        <h1>Login</h1>
-      </div>
+        <div>
+            <Button onClick={handleGoogle}>
+                Sign In with Google
+            </Button>
+        </div>
     )
   }
