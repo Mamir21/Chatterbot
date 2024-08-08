4c1e4a8032b8f841763af741f1a8762a31ea05d2
Muhammad Amir <umairamir1127@gmail.com>
2024-08-07 21:02:33 -0400

    added home

--- app/page.js
+++ app/page.js
@@ -1,11 +1,33 @@
-// import Header from '../components/header';
-// import Footer from '../components/footer';
-// import Home from '../components/homepage';
+'use client';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase';
+import { ProtectedRoute } from './components/ProtectedRoute';
+import { Home } from './home/page';
+ 
+export default function Page() {
+  const [user, loading] = useAuthState(auth);
+  const router = useRouter();
 
-export default function Home() {
+  useEffect(() => {
+    if (!loading && !user) {
+      router.push('/login'); 
+    }
+  }, [user, loading, router]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user) {
+    return <Home />; 
+  }
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+      <ProtectedRoute>
+     {/* Include all the routes that will be protected inside of the ProtectedRoute /}
+     {//chatbot/}
+      {/Any feature that doesn't require the user to SignIn */}
+      </ProtectedRoute>
   )
 }
