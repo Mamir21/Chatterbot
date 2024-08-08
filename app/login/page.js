62786513accd0779a0052216ac1a149fb3a7a05e
Mir Shahiduzzaman <mshahiduzzaman@fordham.edu>
2024-08-08 10:26:48 -0400

    User login/logout
    Note: Need to fix bug where if signed in then refresh, page shows sign in button for a second before showing the sign out button

--- app/login/page.js
+++ app/login/page.js
@@ -2,22 +2,50 @@
 // import Header from '../components/header';
 // import Footer from '../components/footer';
 // import Home from '../components/homepage';
-import { Box, TextField, Button } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Box, TextField, Button, Typography } from "@mui/material";
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import {auth} from '../../firebase.js'
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, signOut } from "firebase/auth";
+import {auth} from '@/firebase'
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function Login() {
+    const [user, setUser] = useAuthState(auth)
+    
+    // useEffect(() => {
+    //     return onAuthStateChanged(auth, (user) => setUser(user))
+    // }, [])
+
     const handleGoogle = async (e) => {
         const provider = new GoogleAuthProvider();
         return signInWithPopup(auth, provider);
     }
 
-    return (
-        <div>
-            <Button onClick={handleGoogle}>
-                Sign In with Google
-            </Button>
-        </div>
-    )
+    const out = async (e) => {
+        return auth.signOut();
+    }
+
+    if (!user) 
+    {
+        return (
+            <div>
+                <Button onClick={handleGoogle}>
+                    Sign In with Google
+                </Button>
+            </div>
+        )
+    }
+    else
+    {
+        return (
+            <div>
+                <Typography>
+                    Signed in
+                </Typography>
+                <Button onClick={out}>
+                    Sign Out
+                </Button>
+            </div>
+        )
+    }
   }
