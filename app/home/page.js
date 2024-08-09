7843e2b7c3d38303928276c17e3209a05e9cd875
Muhammad Amir <umairamir1127@gmail.com>
2024-08-09 10:20:49 -0400

    starting of home

--- app/home/page.js
+++ app/home/page.js
@@ -1,11 +1,11 @@
 // import Header from '../components/header';
 // import Footer from '../components/footer';
-// import Home from '../components/homepage';
+import Homepage from '../components/homepage';
 
 export default function Home() {
     return (
-      <div>
-        <h1>Home</h1>
-      </div>
+      <>
+      <Homepage />
+      </>
     )
   }
