d1b1d458c3834291710acbde1d663b6fcd874588
Muhammad Amir <umairamir1127@gmail.com>
2024-08-09 15:50:02 -0400

    navbar change

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import '../globals.css';
+import '../navbar.css';
 
 export default function Navbar() {
   return (
-    <div className="navbar">
-        <img src='../public/images/logo.png' className="logo" />
+    <nav>
+        <img src='../images/logo.png' className="logo" />
         <ul>
             <li><a href="home">Home</a></li>
-            <li><a href="bot">ChatterBot</a></li>
-            <li><a href="about">About</a></li>
+            <li href="bot">ChatterBot</li>
+            <li href="about">About</li>
         </ul>
         <div>
             <a href='*' className='btn'>Log out</a>
         </div>
-    </div>
+    </nav>
   )
 }
