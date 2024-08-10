1ee4f4f9d8998c34a8ff189a1c5878a4105819c2
Mir Shahiduzzaman <mshahiduzzaman@fordham.edu>
2024-08-10 16:12:44 -0400

    Designed the Log In Page with Colored Boxes

--- app/login/page.js
+++ app/login/page.js
@@ -1,10 +1,11 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import { signInWithGoogle } from '../services/auth';
-import { Button, Box } from '@mui/material';
+import { Button, Box, Typography } from '@mui/material';
 import { auth } from '@/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useEffect } from 'react';
+import cover from "../../public/images/gradient-connection-background.jpg"
 
 export default function Login() {
   const router = useRouter();
@@ -37,20 +38,138 @@ export default function Login() {
   };
 
   return (
-    <Box
-    display={'flex'}
-    justifyContent={'center'}
-    alignItems={'center'}
+    <Box width="100vw" 
+    height="100vh" 
+    display="flex"
+    // need to set background
+    sx={{
+      // backgroundImage: "url('https://lh3.googleusercontent.com/MOf9Kxxkj7GvyZlTZOnUzuYv0JAweEhlxJX6gslQvbvlhLK5_bSTK6duxY2xfbBsj43H=w300')"
+      // backgroundImage: "../../public/images/gradient-connection-background.jpg"
+    }}
     >
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={handleGoogleSignIn}
-        style={{ backgroundColor: '#33292900', color: '#674B4B', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+
+<Box
+      width="250px" 
+      // height="100vh" 
+      border="1px solid #333"
+      borderRadius="8px"
+      bgcolor={'lightblue'}
+      position="absolute"
+      // justifyContent="center"
+      // alignItems="center"
+      left="300px"
+      top="500px"
+      p={1}
+
       >
-        <img src="/images/google-logo.png" alt="Google Logo" style={{ width: '20px', marginRight: '8px', borderRadius: '35%' }} />
-        Sign In with Google
-      </Button>
+        <h1>Bot:</h1>
+        <Typography>-------------------------------------</Typography>
+        <Typography fontSize="30px" position="relative" alignContent="center"
+        alignItems="center"
+        justifyContent="center"
+        justifyItems="center"
+        >
+          Based on the Llama model, we out here with the Chatterbot
+          </Typography>
+      </Box>
+
+      <Box
+      width="250px" 
+      // height="100vh" 
+      border="1px solid #333"
+      borderRadius="8px"
+      bgcolor={'lightblue'}
+      position="absolute"
+      // justifyContent="center"
+      // alignItems="center"
+      left="975px"
+      top="100px"
+      p={1}
+
+      >
+        <h1>Bot2:</h1>
+        <Typography>-------------------------------------</Typography>
+        <Typography fontSize="30px" position="relative" alignContent="center"
+        alignItems="center"
+        justifyContent="center"
+        justifyItems="center"
+        >
+          Based on the Llama model, we out here with the Chatterbot
+          </Typography>
+      </Box>
+
+      <Box
+      width="250px" 
+      // height="100vh" 
+      border="1px solid #333"
+      borderRadius="8px"
+      bgcolor={'lightblue'}
+      position="absolute"
+      // justifyContent="center"
+      // alignItems="center"
+      right="300px"
+      top="500px"
+      p={1}
+
+      >
+        <h1>Bot3:</h1>
+        <Typography>-------------------------------------</Typography>
+        <Typography fontSize="30px" position="relative" alignContent="center"
+        alignItems="center"
+        justifyContent="center"
+        justifyItems="center"
+        >
+          Based on the Llama model, we out here with the Chatterbot
+          </Typography>
+      </Box>
+
+    <Box 
+      width="100vw" 
+      height="100vh" 
+      display="flex" 
+      flexDirection="column"
+      justifyContent="center" 
+      alignItems="center" 
+      gap={2}
+    >
+     <Typography position="relative" fontSize="75px" color="lightblue">Chatterbot</Typography> 
+      <Box
+      // width="100vw" 
+      // height="100vh" 
+      display={'flex'}
+      flexDirection="column"
+      justifyContent={'center'}
+      alignItems={'center'}
+      border="1px solid #333"
+      borderRadius="8px"
+      p={3}
+      bgcolor={'lightblue'}
+      >
+        <h1>Sign Up / Log In</h1>
+        <Typography>---------------------------------------------------------</Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleGoogleSignIn}
+          style={{ backgroundColor: '#33292900', color: '#674B4B', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+        >
+          <img src="/images/google-logo.png" alt="Google Logo" style={{ width: '20px', marginRight: '8px', borderRadius: '35%' }} />
+          Sign In with Google
+        </Button>
+      </Box>
+    </Box>
     </Box>
   );
 }
+
+
+
+// sign out function to be placed before return
+//     const out = async (e) => {
+//         return auth.signOut();
+//     }
+//
+//  Sign Out Button to go in return 
+// <Button onClick={out}>
+//     Sign Out
+// </Button>
